7796abc85e222f5b79e3bbd979d79a80e3e3c8a7
Joe Minichino <joe.minichino@gmail.com>
2026-04-28 15:19:39 +0100

    clean up: remove build output, dev scripts, stale docs and intermediary files

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Lexer } from './lexer';
-export { Parser } from './parser';
-export { TokenType, Token, SourceLocation } from './types';
-export * as AST from './ast';
-//# sourceMappingURL=index.d.ts.map
